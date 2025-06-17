ae9c95c7e9fd8a77218aebea58353c4a738626d3
aleksejpleskanev <priestman0@gmail.com>
2025-06-17 07:57:14 +0300

    small fix

--- src/main/java/one/nio/serial/BigDecimalSerializer.java
+++ src/main/java/one/nio/serial/BigDecimalSerializer.java
@@ -1,11 +1,42 @@
 package one.nio.serial;
 
-import one.nio.util.Utf8;
+import one.nio.serial.gen.NullObjectOutputStream;
+import one.nio.util.JavaInternals;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class BigDecimalSerializer extends Serializer<BigDecimal> {
+
+    private static final MethodHandle INT_VAL_SET;
+    private static final MethodHandle SCALE_SET;
+
+    private static final MethodHandle INT_VAL_GET;
+    private static final MethodHandle SCALE_GET;
+
+    static {
+        try {
+            var look = MethodHandles.privateLookupIn(BigDecimal.class, MethodHandles.lookup());
+
+            Field intVal = BigDecimal.class.getDeclaredField("intVal");
+            intVal.setAccessible(true);
+            INT_VAL_SET = look.unreflectSetter(intVal);
+            INT_VAL_GET = look.unreflectGetter(intVal);
+
+            Field scale = BigDecimal.class.getDeclaredField("scale");
+            scale.setAccessible(true);
+            SCALE_SET = look.unreflectSetter(scale);
+            SCALE_GET = look.unreflectGetter(scale);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     protected BigDecimalSerializer() {
         super(BigDecimal.class);
@@ -13,18 +44,38 @@ public class BigDecimalSerializer extends Serializer<BigDecimal> {
 
     @Override
     public void calcSize(BigDecimal value, CalcSizeStream css) throws IOException {
-        css.count += Utf8.length(value.toString());
+        try {
+            css.writeObject(NullObjectOutputStream.INSTANCE);
+            css.writeObject((BigInteger) INT_VAL_GET.invokeExact(value));
+            css.count += 4;
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void write(BigDecimal obj, DataStream out) throws IOException {
-        out.writeUTF(obj.toString());
+    public void write(BigDecimal value, DataStream out) throws IOException {
+        try {
+//            out.writeObject(NullObjectOutputStream.INSTANCE);
+            out.writeObject((BigInteger) INT_VAL_GET.invokeExact(value));
+            out.writeInt((int) SCALE_GET.invokeExact(value));
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public BigDecimal read(DataStream in) throws IOException, ClassNotFoundException {
-        BigDecimal value = new BigDecimal(in.readUTF());
-        return value;
+        try {
+            BigDecimal value = (BigDecimal) JavaInternals.unsafe.allocateInstance(BigDecimal.class);
+            INT_VAL_SET.invokeExact(value, (BigInteger) in.readObject());
+            SCALE_SET.invokeExact(value, in.readInt());
+            return value;
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
--- src/main/java/one/nio/serial/BigIntegerSerializer.java
+++ src/main/java/one/nio/serial/BigIntegerSerializer.java
@@ -1,99 +1,113 @@
 package one.nio.serial;
 
-import one.nio.util.Utf8;
+import one.nio.util.JavaInternals;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Field;
 import java.math.BigInteger;
 
 public class BigIntegerSerializer extends Serializer<BigInteger> {
 
-//    private static final VarHandle MODIFIERS;
-//    private static final VarHandle SIGNUM;
-//    private static final VarHandle MAG;
-//    private static final VarHandle BIT_COUNT_PLUS_ONE;
-//    private static final VarHandle BIT_LENGTH_PLUS_ONE;
-//    private static final VarHandle LOWEST_SET_BIT_PLUS_TWO;
-//    private static final VarHandle FIRST_NONZERO_INT_NUM_PLUS_TWO;
-//
-//    static {
-//        try {
-//            var look = MethodHandles.privateLookupIn(Field.class, MethodHandles.lookup());
-////            MODIFIERS = look.findVarHandle(Field.class, "modifiers", int.class);
-//            MethodHandles.Lookup lookup = MethodHandles.lookup();
-////            Field modifiersField = Field.class.getDeclaredField("modifiers");
-//
-//            Field signum = BigInteger.class.getDeclaredField("signum");
-////            var t = look.unreflectSetter(signum);
-////            t.invoke(signum, signum.getModifiers() & ~FINAL);
-//
-//            signum.setAccessible(true);
-////            signum.setInt(signum, signum.getModifiers() & ~FINAL);
-////            MODIFIERS.set(signum, signum.getModifiers() & ~Modifier.FINAL);
-//            SIGNUM = MethodHandles.privateLookupIn(BigInteger.class, MethodHandles.lookup()).unreflectVarHandle(signum);
-//
-//            Field mag = BigInteger.class.getDeclaredField("mag");
-//            mag.setAccessible(true);
-////            mag.setInt(signum, signum.getModifiers() & ~FINAL);
-//            MAG = lookup.unreflectVarHandle(mag);
-//
-//            Field bitCountPlusOne = BigInteger.class.getDeclaredField("bitCountPlusOne");
-//            bitCountPlusOne.setAccessible(true);
-//            bitCountPlusOne.setInt(signum, signum.getModifiers() & ~FINAL);
-//            BIT_COUNT_PLUS_ONE = lookup.unreflectVarHandle(mag);
-//
-//            Field bitLengthPlusOne = BigInteger.class.getDeclaredField("bitLengthPlusOne");
-//            bitLengthPlusOne.setAccessible(true);
-//            bitLengthPlusOne.setInt(signum, signum.getModifiers() & ~FINAL);
-//            BIT_LENGTH_PLUS_ONE = lookup.unreflectVarHandle(mag);
-//
-//            Field lowestSetBitPlusTwo = BigInteger.class.getDeclaredField("lowestSetBitPlusTwo");
-//            lowestSetBitPlusTwo.setAccessible(true);
-//            lowestSetBitPlusTwo.setInt(signum, signum.getModifiers() & ~FINAL);
-//            LOWEST_SET_BIT_PLUS_TWO = lookup.unreflectVarHandle(mag);
-//
-//            Field firstNonzeroIntNumPlusTwo = BigInteger.class.getDeclaredField("firstNonzeroIntNumPlusTwo");
-//            firstNonzeroIntNumPlusTwo.setAccessible(true);
-//            firstNonzeroIntNumPlusTwo.setInt(signum, signum.getModifiers() & ~FINAL);
-//            FIRST_NONZERO_INT_NUM_PLUS_TWO = lookup.unreflectVarHandle(mag);
-//
+    private static final MethodHandle SIGNUM_SET;
+    private static final MethodHandle MAG_SET;
+    private static final MethodHandle BIT_COUNT_PLUS_ONE_SET;
+    private static final MethodHandle BIT_LENGTH_PLUS_ONE_SET;
+    private static final MethodHandle LOWEST_SET_BIT_PLUS_TWO_SET;
+    private static final MethodHandle FIRST_NONZERO_INT_NUM_PLUS_TWO_SET;
 
-    /// /            SIGNUM = MethodHandles.privateLookupIn(BigInteger.class, MethodHandles.lookup())
-    /// /                    .findVarHandle(BigInteger.class, "signum", int.class);
-    /// /            MAG = MethodHandles.privateLookupIn(BigInteger.class, MethodHandles.lookup())
-    /// /                    .findVarHandle(BigInteger.class, "mag", int[].class);
-    /// /            BIT_COUNT_PLUS_ONE = MethodHandles.privateLookupIn(BigInteger.class, MethodHandles.lookup())
-    /// /                    .findVarHandle(BigInteger.class, "bitCountPlusOne", int.class);
-    /// /            BIT_LENGTH_PLUS_ONE = MethodHandles.privateLookupIn(BigInteger.class, MethodHandles.lookup())
-    /// /                    .findVarHandle(BigInteger.class, "bitLengthPlusOne", int.class);
-    /// /            LOWEST_SET_BIT_PLUS_TWO = MethodHandles.privateLookupIn(BigInteger.class, MethodHandles.lookup())
-    /// /                    .findVarHandle(BigInteger.class, "lowestSetBitPlusTwo", int.class);
-    /// /            FIRST_NONZERO_INT_NUM_PLUS_TWO = MethodHandles.privateLookupIn(BigInteger.class, MethodHandles.lookup())
-    /// /                    .findVarHandle(BigInteger.class, "firstNonzeroIntNumPlusTwo", int.class);
-    /// /            Field field = BigInteger.class.getDeclaredField("signum");
-//        } catch (ReflectiveOperationException e) {
-//            throw new ExceptionInInitializerError(e);
-//        } catch (Throwable e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    private static final MethodHandle SIGNUM_GET;
+    private static final MethodHandle MAG_GET;
+    private static final MethodHandle BIT_COUNT_PLUS_ONE_GET;
+    private static final MethodHandle BIT_LENGTH_PLUS_ONE_GET;
+    private static final MethodHandle LOWEST_SET_BIT_PLUS_TWO_GET;
+    private static final MethodHandle FIRST_NONZERO_INT_NUM_PLUS_TWO_GET;
+
+    static {
+        try {
+            var look = MethodHandles.privateLookupIn(BigInteger.class, MethodHandles.lookup());
+
+            Field signum = BigInteger.class.getDeclaredField("signum");
+            signum.setAccessible(true);
+            SIGNUM_SET = look.unreflectSetter(signum);
+            SIGNUM_GET = look.unreflectGetter(signum);
+
+            Field mag = BigInteger.class.getDeclaredField("mag");
+            mag.setAccessible(true);
+            MAG_SET = look.unreflectSetter(mag);
+            MAG_GET = look.unreflectGetter(mag);
+
+            Field bitCountPlusOne = BigInteger.class.getDeclaredField("bitCountPlusOne");
+            bitCountPlusOne.setAccessible(true);
+            BIT_COUNT_PLUS_ONE_SET = look.unreflectSetter(bitCountPlusOne);
+            BIT_COUNT_PLUS_ONE_GET = look.unreflectGetter(bitCountPlusOne);
+
+            Field bitLengthPlusOne = BigInteger.class.getDeclaredField("bitLengthPlusOne");
+            bitLengthPlusOne.setAccessible(true);
+            BIT_LENGTH_PLUS_ONE_SET = look.unreflectSetter(bitLengthPlusOne);
+            BIT_LENGTH_PLUS_ONE_GET = look.unreflectGetter(bitLengthPlusOne);
+
+            Field lowestSetBitPlusTwo = BigInteger.class.getDeclaredField("lowestSetBitPlusTwo");
+            lowestSetBitPlusTwo.setAccessible(true);
+            LOWEST_SET_BIT_PLUS_TWO_SET = look.unreflectSetter(lowestSetBitPlusTwo);
+            LOWEST_SET_BIT_PLUS_TWO_GET = look.unreflectGetter(lowestSetBitPlusTwo);
+
+            Field firstNonzeroIntNumPlusTwo = BigInteger.class.getDeclaredField("firstNonzeroIntNumPlusTwo");
+            firstNonzeroIntNumPlusTwo.setAccessible(true);
+            FIRST_NONZERO_INT_NUM_PLUS_TWO_SET = look.unreflectSetter(firstNonzeroIntNumPlusTwo);
+            FIRST_NONZERO_INT_NUM_PLUS_TWO_GET = look.unreflectGetter(firstNonzeroIntNumPlusTwo);
+        } catch (ReflectiveOperationException e) {
+            throw new ExceptionInInitializerError(e);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     protected BigIntegerSerializer() {
         super(BigInteger.class);
     }
 
     @Override
     public void calcSize(BigInteger value, CalcSizeStream css) throws IOException {
-        css.count += Utf8.length(value.toString());
+        try {
+            css.writeObject((int[]) MAG_GET.invokeExact(value));
+            css.count += 20;
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void write(BigInteger value, DataStream out) throws IOException {
-        out.writeUTF(value.toString());
+        try {
+            out.writeInt((int) SIGNUM_GET.invokeExact(value));
+            out.writeObject((int[]) MAG_GET.invokeExact(value));
+            out.writeInt((int) BIT_COUNT_PLUS_ONE_GET.invokeExact(value));
+            out.writeInt((int) BIT_LENGTH_PLUS_ONE_GET.invokeExact(value));
+            out.writeInt((int) LOWEST_SET_BIT_PLUS_TWO_GET.invokeExact(value));
+            out.writeInt((int) FIRST_NONZERO_INT_NUM_PLUS_TWO_GET.invokeExact(value));
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public BigInteger read(DataStream in) throws IOException, ClassNotFoundException {
-        BigInteger value = new BigInteger((String) in.readUTF());
-        return value;
+        try {
+            BigInteger value = (BigInteger) JavaInternals.unsafe.allocateInstance(BigInteger.class);
+            SIGNUM_SET.invokeExact(value, in.readInt());
+            MAG_SET.invokeExact(value, (int[]) in.readObject());
+            BIT_COUNT_PLUS_ONE_SET.invokeExact(value, in.readInt());
+            BIT_LENGTH_PLUS_ONE_SET.invokeExact(value, in.readInt());
+            LOWEST_SET_BIT_PLUS_TWO_SET.invokeExact(value, in.readInt());
+            FIRST_NONZERO_INT_NUM_PLUS_TWO_SET.invokeExact(value, in.readInt());
+            return value;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
